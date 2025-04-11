75850163b83f05520754796dcee89e44a26e7fa3
Ana Paula Lopes <131221033+paulalopez-git@users.noreply.github.com>
2025-04-11 23:52:04 +0000

    Exibição dados da segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    
     // Declaração das variáveis para a primeira carta
     char estado1;
     char codigo1[4];
@@ -18,7 +19,6 @@ int main() {
     float area2;
     float pib2;
     int pontosTuristicos2;
-     
      
     // Entrada dos dados da primeira carta
     printf("Digite o estado da primeira carta (A-H): ");
@@ -65,8 +65,15 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
-
-
+    // Exibição dos dados da segunda carta
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
     return 0;
 }
