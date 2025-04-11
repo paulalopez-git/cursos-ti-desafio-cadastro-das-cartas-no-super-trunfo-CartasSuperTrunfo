2e53af5e1b554f54c29629adbd38e8a5296f323f
Ana Paula Lopes <131221033+paulalopez-git@users.noreply.github.com>
2025-04-11 23:33:16 +0000

    Definindo variáveis para cadastro segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,15 @@ int main() {
     float pib1;
     int pontosTuristicos1;
 
+     // Declaração das variáveis para a segunda carta
+     char estado2;
+     char codigo2[4];
+     char nomeCidade2[50];
+     int populacao2;
+     float area2;
+     float pib2;
+     int pontosTuristicos2;
+ 
 
     return 0;
 }
